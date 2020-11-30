965eaba9b93cfb6850c690cc78f32d28e086087b
Omar Abdel-Ghani <64589450+Omar622@users.noreply.github.com>
2020-11-30 14:07:41 +0200

    Create J. The one when Safrout narrates Mafia.cpp

---
+++ ECPC 19/J. The one when Safrout narrates Mafia.cpp
@@ -0,0 +1,47 @@
+       /* Do it! */
+      /*  OMAR  */
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void init(){
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+}
+
+int n;
+string arr[3000], mem[3000];
+
+string dp(int i){
+    if(i == n)
+        return "";
+
+    string& ret = mem[i];
+
+    if(ret != "")
+        return ret;
+
+    string ch = dp(i+1);
+    ret = max(arr[i] + ch, ch);
+
+    return ret;
+}
+
+int main(){
+//    o:
+    freopen("lex.in", "r", stdin);
+    init();
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for (int i = 0; i < n; i++){
+            cin>>arr[i];
+            mem[i] = "";
+        }
+        cout<<dp(0)<<"\n";
+    }
+//    goto o;
+    return 0;
+}
